3f0562abe90f3afe150a6324918ddc55d936bd04
cherishs001 <cherish@kaishens.cn>
2022-04-26 17:15:37 +0800

    test: 补全reducer相关的测试用例

--- __test__/reducer.spec.tsx
+++ __test__/reducer.spec.tsx
@@ -13,18 +13,28 @@ const TextView = (props: ReduxStoreProps) => {
         }}>
             按钮
         </div>
-        <div>{props.state.global.id}</div>
-        <div>{props.state.global.name}</div>
+        <div>{props.state?.global?.id}</div>
+        <div>{props.state?.global?.name}</div>
     </>;
 }
 
-const TextViewConnect = store.connect(TextView);
+const TextViewConnect = store.connect(TextView, ['global']);
 store.addModel(Global);
 
 test('在reducer中重复调用reducer的场景', () => {
     render(store.start(<TextViewConnect/>));
 
     fireEvent.click(screen.getByText('按钮'));
-    expect(screen.getByText('888')).not.toBeNull();
-    expect(screen.getByText('kai')).not.toBeNull();
+    expect(screen.queryByText('888')).not.toBeNull();
+    expect(screen.queryByText('kai')).not.toBeNull();
+})
+
+const TextViewConnectOfSingleString = store.connect(TextView, 'global');
+
+test('单namespace时的connect', () => {
+    render(store.start(<TextViewConnectOfSingleString/>));
+
+    fireEvent.click(screen.getByText('按钮'));
+    expect(screen.queryByText('888')).toBeNull();
+    expect(screen.queryByText('kai')).toBeNull();
 })
